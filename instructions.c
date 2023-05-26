c4e39e09b1a87d647c8b09fea87864fd84ee9fee
Uceetheblackone <anyanwuuchechukwujay@gmail.com>
2023-05-26 04:38:27 -0700

    update

--- instructions.c
+++ instructions.c
@@ -3,58 +3,52 @@
 /**
  * execute - used to run operation codes
  * @content: content
- * @stack: double pointer to the head of the stack
+ * @head: double pointer to the head of the stack
  * @counter: counter
  * @file: file pointer
- * Return: Nothing
+ * Return: 1 on success.
  */
 int execute(char *content, stack_t **head, unsigned int counter, FILE *file)
 {
-    instruction_t opst[] = {
-        {"pint", &pint},
-        {"pop", &f_pop},
-        {"swap", &f_swap},
-        {"add", &f_add},
-	{"sub", &f_sub},
-	{"div", &f_div},
-	{"mod", &f_mod},
-	{"pchar", &f_pchar},
-	{"pstr", &f_pstr},
-	{"mul", &f_mul},
-        {"nop", &f_nop},
-        {"push", &f_push},
-        {"pall", &f_pall},
-        {NULL, NULL}
-    };
+	instruction_t opst[] = {
+		{"pint", &pint},
+		{"pop", &f_pop},
+		{"swap", &f_swap},
+		{"add", &f_add},
+		{"sub", &f_sub},
+		{"div", &f_div},
+		{"mod", &f_mod},
+		{"pchar", &f_pchar},
+		{"pstr", &f_pstr},
+		{"mul", &f_mul},
+		{"nop", &f_nop},
+		{"push", &f_push},
+		{"pall", &f_pall},
+		{NULL, NULL}
+	};
+	unsigned int i = 0;
+	char *op;
 
-    unsigned int i = 0;
-    char *op;
-
-    op = strtok(content, " \n\t");
-    if (op && op[0] == '#')
-        return (0);
-
-    bus.arg = strtok(NULL, " \n\t");
-
-    while (opst[i].opcode && op)
-    {
-        if (strcmp(op, opst[i].opcode) == 0)
-        {
-            opst[i].f(head, counter);
-            return (0);
-        }
-        i++;
-    }
-
-    if (op && opst[i].opcode == NULL)
-    {
-        fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
-        fclose(file);
-        free(content);
-        free_stack(*head);
-        exit(EXIT_FAILURE);
-    }
-
-    return (1);
+	op = strtok(content, " \n\t");
+	if (op && op[0] == '#')
+		return (0);
+	bus.arg = strtok(NULL, " \n\t");
+	while (opst[i].opcode && op)
+	{
+		if (strcmp(op, opst[i].opcode) == 0)
+		{
+			opst[i].f(head, counter);
+			return (0);
+		}
+		i++;
+	}
+	if (op && opst[i].opcode == NULL)
+	{
+		fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
+		fclose(file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	return (1);
 }
-
